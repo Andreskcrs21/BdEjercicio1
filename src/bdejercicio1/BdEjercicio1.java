43e0b4e00753bde09c8e6b3eb2ad465d3597ce7e
Andreskcrs_21 <cacaceres@tecnologicoloja.edu.ec>
2021-01-06 18:03:32 -0500

    Tarea

---
+++ src/bdejercicio1/BdEjercicio1.java
@@ -0,0 +1,39 @@
+
+package bdejercicio1;
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+public class BdEjercicio1 {
+    private final String base = "bdEjercicio1";
+    private final String user = "root";
+    private final String password = "chateo282";
+    private final String url = "jdbc:mysql://Localhost:3306/" + base;
+    PreparedStatement ps;
+
+    public Connection getConexion() {
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = (Connection) DriverManager.getConnection(url, user, password);
+            //JOptionPane.showMessageDialog(null, "Conexion exitosa");
+            System.out.println("Conexion Establecida");
+        } catch (ClassNotFoundException ex) {
+            JOptionPane.showMessageDialog(null, "Error de Conexion");
+        } catch (SQLException ex) {
+            
+        }
+        return con;
+    }
+    
+    public static void main(String[] args) {
+        BdEjercicio1 con = new BdEjercicio1();
+        con.getConexion();
+    }
+    
+    
+}
+        
