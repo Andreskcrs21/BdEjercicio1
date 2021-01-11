33b67d34605c48a3ae6ea0ecbc94f78a705d1841
Andreskcrs21 <68607025+Andreskcrs21@users.noreply.github.com>
2021-01-10 21:47:58 -0500

    Add files via upload

---
+++ src/bdejercicio1/Persona.java
@@ -0,0 +1,84 @@
+
+package com.istloja.modelo;
+
+/**
+ *
+ * @author IdeaPad - S340
+ */
+public class Persona {
+    private int idPersona;
+    private String cedula;
+    private String nombres;
+    private String apellidos;
+    private String direccion;
+    private String correo;
+    private String telefono;
+
+    /*public Persona(int idPersona, String cedula, String nombres, String apellidos, String direccion, String correo, String telefono) {
+        this.idPersona = idPersona;
+        this.cedula = cedula;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.direccion = direccion;
+        this.correo = correo;
+        this.telefono = telefono;
+    */
+
+    
+
+    public int getIdPersona() {
+        return idPersona;
+    }
+
+    public void setIdPersona(int idPersona) {
+        this.idPersona = idPersona;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
